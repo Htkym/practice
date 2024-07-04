baa04c2ca69a7288abc0df678b6d93a9e53fcb05
HiroshiTakayama <thiroshi.3088@gmail.com>
2024-07-04 22:03:11 +0900

    # Create Command

---
+++ Calculator/Calculator/Commands/RelayCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Input;
+
+namespace Calculator.Commands;
+
+public class RelayCommand(Action<object> executeAction, Func<object, bool> canExecute = null) : ICommand
+{
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter)
+        => canExecute?.Invoke(parameter) ?? true;
+
+    public void Execute(object parameter)
+        => executeAction?.Invoke(parameter);
+}
