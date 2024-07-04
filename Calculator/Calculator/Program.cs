440ea76388f09192c08f1c1dde4ce597686986ab
HiroshiTakayama <thiroshi.3088@gmail.com>
2024-07-04 22:11:29 +0900

    # Set Logger And ErrorHandler

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -1,28 +1,47 @@
+using Calculator.Presenters;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-namespace Calculator
+namespace Calculator;
+
+internal static class Program
 {
-    internal static class Program
+    public static IServiceProvider ServiceProvider { get; private set; } = default!;
+
+    [STAThread]
+    private static void Main()
     {
-        public static IServiceProvider ServiceProvider { get; private set; } = default!;
+        Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+        AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
 
-        /// <summary>
-        ///  The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            ServiceProvider = CreateHostBuilder().Build().Services;
-            Application.Run(ServiceProvider.GetService<Form1>()
-                ?? throw new ArgumentNullException("Cannot Run Application"));
-        }
+        ApplicationConfiguration.Initialize();
+        ServiceProvider = CreateHostBuilder()
+            .ConfigureLogging((context, logging) =>
+            {
+                logging.ClearProviders();
+                logging.AddLog4Net(log4NetConfigFile: "log4net.config");
+                logging.AddConsole();
+                logging.AddDebug();
+            })
+            .Build()
+            .Services;
 
-        static IHostBuilder CreateHostBuilder()
-            => Host.CreateDefaultBuilder()
-                   .ConfigureServices((c, s) => s.AddServices(c));
+        var presenter = ServiceProvider.GetService<CalculatorPresenter>();
+        Application.Run(presenter.View
+            ?? throw new ArgumentNullException("Cannot Run Application"));
     }
+
+    static IHostBuilder CreateHostBuilder()
+        => Host.CreateDefaultBuilder()
+               .ConfigureServices((c, s) => s.AddServices(c));
+
+    static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        => ShowExceptionDetails(e.Exception);
+
+    static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        => ShowExceptionDetails(e.ExceptionObject as Exception);
+
+    static void ShowExceptionDetails(Exception ex)
+        => MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 }
